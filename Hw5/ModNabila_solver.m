08214eec95baeeee381af40f47ff64b63b71f3d6
Psycho-Wolf <antony_pavel@hotmail.com>
2021-03-10 00:33:45 -0700

    Without Inductance working

--- Hw5/ModNabila_solver.m
+++ Hw5/ModNabila_solver.m
@@ -5,7 +5,7 @@ clc
 h=0.001; %simulation time step
 t=0:h:5; %time array
 b=zeros(6,length(t)); %initialize state vector
-F=zeros(3,length(t)); %initialize generalized force vector
+V=zeros(3,length(t)); %initialize generalized force vector
 b(:,1)=[0;... %initial theta1
  0;... %initial theta2
  0;...%initial theta3
@@ -13,16 +13,16 @@ b(:,1)=[0;... %initial theta1
  0;... %initial dottheta2
  0];%initial dottheta3
 
-F(1,:)=.01*ones(1,length(t)); %0.01 nm torque applied to link 1
-F(2,:)=.005*ones(1,length(t)); %0.005 nm torque applied to link 2
-F(3,:)=.005*ones(1,length(t));
+V(1,:)=12*ones(1,length(t)); %0.01 nm torque applied to link 1
+V(2,:)=12*ones(1,length(t)); %0.005 nm torque applied to link 2
+V(3,:)=12*ones(1,length(t));
 
 %---Solve Equations of Motion---
 for i=1:length(t)-1
- k1=ModNabila(b(:,i),F(:,i));
- k2=ModNabila(b(:,i)+k1*h/2,F(:,i));
- k3=ModNabila(b(:,i)+k2*h/2,F(:,i));
- k4=ModNabila(b(:,i)+k3*h,F(:,i));
+ k1=Nabila_Motor_woL(b(:,i),V(:,i));
+ k2=Nabila_Motor_woL(b(:,i)+k1*h/2,V(:,i));
+ k3=Nabila_Motor_woL(b(:,i)+k2*h/2,V(:,i));
+ k4=Nabila_Motor_woL(b(:,i)+k3*h,V(:,i));
  b(:,i+1)=b(:,i)+h*(k1/6+k2/3+k3/3+k4/6);
 end
 %---Data Plotting---
